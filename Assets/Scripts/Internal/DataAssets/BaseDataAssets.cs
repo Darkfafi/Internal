cc03955f8b4c19d21de2f812851aee091f303ae9
Darkfafi <ik_ben_ramses@hotmail.com>
2019-05-06 21:10:55 +0200

    Created DataAssets (Listed and non listed)

---
+++ Assets/Scripts/Internal/DataAssets/BaseDataAssets.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace DataAssets
+{
+	public abstract class BaseDataAssets<SELF, Data> : ScriptableObject, IAssets<Data> where SELF : UnityEngine.Object, IAssets<Data>
+	{
+		public const string ASSETS_RESOURCE_PATH = "DataAssets/";
+
+		public static SELF Instance
+		{
+			get
+			{
+				if(_cachedAssets == null)
+				{
+					if(_cachedAssets == null)
+					{
+						_cachedAssets = ResourceLocator.Locate<SELF>("", ASSETS_RESOURCE_PATH);
+					}
+
+					if(_cachedAssets == null)
+					{
+						throw new Exception($"No `{typeof(SELF).Name}` Assets object found at Resource location: " + ASSETS_RESOURCE_PATH);
+					}
+					else
+					{
+						if(!_cachedAssets.RefreshCache(out string message))
+						{
+							throw new Exception(message);
+						}
+					}
+				}
+
+				return _cachedAssets;
+			}
+		}
+
+		private static SELF _cachedAssets = null;
+
+		public abstract bool TryGetAsset(string assetID, out Data data);
+		public abstract bool HasAsset(string assetID);
+		public virtual bool RefreshCache(out string message)
+		{
+			message = string.Empty;
+			return true;
+		}
+	}
+
+	public interface IAssets<Data>
+	{
+		bool TryGetAsset(string assetID, out Data data);
+		bool HasAsset(string assetID);
+		bool RefreshCache(out string message);
+	}
+}
---
+++ Assets/Scripts/Internal/DataAssets/BaseListedDataAssets.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataAssets
+{
+	public abstract class BaseListedDataAssets<SELF, Data> : BaseDataAssets<SELF, Data> where Data : IListedAssetData where SELF : Object, IAssets<Data>
+	{
+		[SerializeField]
+		private Data[] _assetsData = null;
+
+		private Dictionary<string, Data> _assetsCached;
+
+		public override bool RefreshCache(out string message)
+		{
+			_assetsCached = new Dictionary<string, Data>();
+			message = string.Empty;
+			for(int i = 0; i < _assetsData.Length; i++)
+			{
+				Data data = _assetsData[i];
+				if(_assetsCached.ContainsKey(data.AssetID))
+				{
+					message = $"Already detected Asset with ID {data.AssetID}! Process canceled!";
+					return false;
+				}
+
+				_assetsCached[data.AssetID] = data;
+			}
+			return true;
+		}
+
+		public override bool TryGetAsset(string assetID, out Data data)
+		{
+			return _assetsCached.TryGetValue(assetID, out data);
+		}
+
+		public override bool HasAsset(string assetID)
+		{
+			return _assetsCached.ContainsKey(assetID);
+		}
+	}
+
+	public interface IListedAssetData
+	{
+		string AssetID
+		{
+			get;
+		}
+	}
+}
