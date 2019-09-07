e0a5a645093f02353dee928f071c261a3ae51224
Darkfafi <ik_ben_ramses@hotmail.com>
2019-09-08 00:41:37 +0200

    Created IEnumeratorUtils with Async start coroutine.

---
+++ Assets/Scripts/Internal/Utils/IEnumeratorUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+
+public static class IEnumeratorUtils
+{
+	public static async void StartAsync(this IEnumerator enumerator)
+	{
+		while(enumerator.MoveNext())
+		{
+			await Task.Delay(1);
+		}
+		enumerator = null;
+	}
+}
