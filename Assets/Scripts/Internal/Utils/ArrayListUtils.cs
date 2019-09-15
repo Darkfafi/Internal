f28936107ec1e59c3e681a08a0795f0726938abe
Darkfafi <ik_ben_ramses@hotmail.com>
2019-09-15 13:03:01 +0200

    Added Array and List Utils.

---
+++ Assets/Scripts/Internal/Utils/ArrayListUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public static class ArrayListUtils
+{
+	public static bool InBounds<T>(this T[] array, int index)
+	{
+		return index >= 0 && index < array.Length;
+	}
+
+	public static bool InBounds<T>(this List<T> list, int index)
+	{
+		return index >= 0 && index < list.Count;
+	}
+}
